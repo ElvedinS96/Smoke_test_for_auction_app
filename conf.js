0f03faf9e3d22cc8dd978b2794b957fcccf31759
ElvedinS96 <e.suljic96@gmail.com>
2020-11-15 13:39:27 +0100

    Resolved comments

--- conf.js
+++ conf.js
@@ -1,9 +1,13 @@
 exports.config = {
     framework : "jasmine",
     seleniumAddress: 'http://localhost:4444/wd/hub',
+<<<<<<< HEAD
     specs: [//'./Tests/smokeTest.js'
     "./Tests/regressionTests.js"
   ],
+=======
+    specs: ["./Tests/regressionTests.js"],
+>>>>>>> e0b9158... Added data page
     jasmineNodeOpts: {
       defaultTimeoutInterval: 2500000
       }
