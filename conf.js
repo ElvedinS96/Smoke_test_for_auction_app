4f3b6f2bc6bbff77f07508826a6116d72d10a2ab
ElvedinS96 <e.suljic96@gmail.com>
2020-11-27 22:28:42 +0100

    Removed selenium address

--- conf.js
+++ conf.js
@@ -1,7 +1,6 @@
 exports.config = {
     directConnect: true,
     framework: 'jasmine',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['./Tests/smokeTest.js'],
     jasmineNodeOpts: {
       defaultTimeoutInterval: 2500000
