0a28428a109e6fa2efc8136fa0b5455ca11e5cc6
ElvedinS96 <e.suljic96@gmail.com>
2020-11-28 23:26:37 +0100

    Editing test cases for Jenkins

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 exports.config = {
-    /* directConnect: true, */
-    seleniumAddress: 'http://localhost:4444/wd/hub',
+    directConnect: true,
+    //seleniumAddress: 'http://localhost:4444/wd/hub',
     framework : "jasmine",
     specs: ["./Tests/biddingTests.js",
     "./Tests/aboutUsTests.js",
