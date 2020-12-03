2bfaffdc5f2871308015e61fe511646be7fb5827
Elvedin <56048342+ElvedinS96@users.noreply.github.com>
2020-12-03 17:00:15 +0100

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,8 +1,28 @@
 exports.config = {
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    framework: 'jasmine',
-    specs: ['./Tests/smokeTest.js'],
-    jasmineNodeOpts: {
-      defaultTimeoutInterval: 2500000
-      }
-  }
+  framework : "jasmine",
+  specs: ["./Tests/biddingTests.js",
+  "./Tests/aboutUsTests.js",
+  "./Tests/loginTests.js",
+  "./Tests/privacyAndPolicyTests.js",
+  "./Tests/registrationTests.js",
+  "./Tests/termsAndConditionsTests.js",
+  "./Tests/newArrivalsTest.js",
+  "./Tests/topRatedTest.js",
+  "./Tests/lastChanceTest.js",
+  "./Tests/shopTests.js",
+  "./Tests/featureCollectionTest.js",
+  "./Tests/featureProductsTest.js",
+  "./Tests/searchBarTests.js",
+  "./Tests/myAccountTests.js"
+],
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 2500000
+    },
+   /* capabilities: {
+      browserName: 'chrome',
+    
+    chromeOptions: {
+        args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+       }
+  } */
+}
