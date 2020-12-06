01a7667d88c0d45aeeac40d4fff08d37c0b60c61
ElvedinS96 <e.suljic96@gmail.com>
2020-12-06 14:56:18 +0100

    Update feature products test suite

--- Tests/featureProductsTest.js
+++ Tests/featureProductsTest.js
@@ -10,7 +10,6 @@ describe("011: Feature Products", function(){
     it("001: User is able to open item from 'Feature Products' section", function(){
         homePage.openPageURL(data.homepageLink)
             .then(() => homePage.clickOnElement(data.featureCollectionProductsTitle))
-            .then(() => itemPage.validateBidElements(data.booleanFalse))
-
+            .then(() => itemPage.validateBidElements(false))
     })
 })
